9353c0382dd3228b47903e9dca301e1092c055a5
StrawaCUA <70916394+StrawaCUA@users.noreply.github.com>
2020-09-18 15:49:45 -0400

    Can detect some overlaps, but it isn't consistent. Might need a new algorithm for much better consistency, as this is probably the best this method will do.

--- segments.py
+++ segments.py
@@ -6,10 +6,13 @@ class Point:
 
 def intersect(a0, a1, b0, b1):
     point_intersect_coords = intersect_point(a0, a1, b0, b1)
-    if point_intersect_coords is None:
+    if point_intersect_coords is None and on_line(a0, a1, b0):
+        return on_segment(a0, a1, b0) or on_segment(a0, a1, b1) or on_segment(b0, b1, a0) or on_segment(b0, b1, a1)
+    elif point_intersect_coords is None:
         return False
+
     point_intersect = Point(point_intersect_coords[0], point_intersect_coords[1])
-    if on_line(a0, a1, point_intersect) and on_line(b0, b1, point_intersect):
+    if on_segment(a0, a1, point_intersect) and on_segment(b0, b1, point_intersect):
         return True
     return False
 
@@ -33,7 +36,15 @@ def intersect_point(p0, p1, q0, q1):
     return [x, y]
 
 
-def on_line(p0, p1, i):
+def on_line(p0, p1, q):
+    # Lines in for Ax + By = C
+    a1 = p1.y - p0.y
+    b1 = p1.x - p0.x
+    c1 = a1 * p0.x + b1 * p0.y
+    return a1 * q.x + b1 * q.y == c1
+
+
+def on_segment(p0, p1, i):
     x_min = p0.x
     x_max = p1.x
     y_min = p0.y
@@ -51,8 +62,8 @@ def on_line(p0, p1, i):
     return False
 
 
-point_a0 = Point(-1, 0)
-point_a1 = Point(1, 0)
-point_b0 = Point(-14, 0)
-point_b1 = Point(14, 0)
+point_a0 = Point(0, 0)
+point_a1 = Point(0, -2)
+point_b0 = Point(0, -1)
+point_b1 = Point(0, 10)
 print(intersect(point_a0, point_a1, point_b0, point_b1))
